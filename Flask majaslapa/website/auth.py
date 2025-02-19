c6de0584fb6d58ecb94ee8003d83e8e4bf90a3c2
Liva-Eglite <151825206+Liva-Eglite@users.noreply.github.com>
2025-02-19 10:25:15 +0200

    Update auth.py

--- Flask majaslapa/website/auth.py
+++ Flask majaslapa/website/auth.py
@@ -6,6 +6,10 @@ from flask_login import login_user, login_required, logout_user, current_user
 
 auth = Blueprint('auth', __name__)
 
+@auth.route('/')
+def welcome():
+    return render_template('index1.html')
+
 @auth.route('/login', methods=['GET','POST'])
 def login():
     if request.method == 'POST':
